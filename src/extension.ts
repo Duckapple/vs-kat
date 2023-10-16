e78e2659f3c0db96c6d098b51ebbf0fac2138697
Simon Green Kristensen <git@simon-green.dev>
2023-10-16 21:32:44 +0200

    Add testing skeleton

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,19 @@
-import { commands, ExtensionContext } from "vscode";
+import {
+  commands,
+  ExtensionContext,
+  TestRunProfileKind,
+  tests,
+  workspace,
+  window,
+  Uri,
+  TestItem,
+} from "vscode";
 import { HelloWorldPanel } from "./panels/HelloWorldPanel";
+import { parseRC, printRC } from "./utilities/handleRcFile";
+import { readdir } from "fs/promises";
+import { existsSync } from "fs";
 
-export function activate(context: ExtensionContext) {
+export async function activate(context: ExtensionContext) {
   // Create the show hello world command
   const showHelloWorldCommand = commands.registerCommand(
     "vs-kat.showHelloWorld",
@@ -10,6 +22,117 @@ export function activate(context: ExtensionContext) {
     }
   );
 
+  const file = await workspace.openTextDocument(
+    workspace.workspaceFolders?.[0].uri.path + "/.kattisrc"
+  );
+
+  const text = file.getText();
+
+  console.log(text);
+  const rc = parseRC(text);
+  console.log(printRC(rc));
+
+  if (rc.kattis?.loginurl) console.log(rc.kattis.loginurl);
+
+  const testController = tests.createTestController(
+    "vs-kat.tests",
+    "VS Kat Tests"
+  );
+
+  async function resolveHandler(item?: TestItem) {
+    if (!item || !item.canResolveChildren || item.children.size > 0) return;
+
+    const testDir = item.uri?.fsPath;
+    if (!testDir) return;
+    item.busy = true;
+
+    if (existsSync(testDir)) {
+      for (const testFile of await readdir(testDir, { encoding: "utf-8" })) {
+        if (!testFile.endsWith(".in")) continue;
+        item.children.add(
+          testController.createTestItem(
+            item.id + "." + testFile,
+            testFile,
+            Uri.file(testDir + "/" + testFile)
+          )
+        );
+      }
+    }
+
+    item.busy = false;
+  }
+
+  testController.resolveHandler = resolveHandler;
+
+  const testProfile = testController.createRunProfile(
+    "VS Kat: Active Problems",
+    TestRunProfileKind.Run,
+    async (request, cancel) => {
+      const run = testController.createTestRun(request);
+      for (const item of request.include ?? []) {
+        run.enqueued(item);
+        await resolveHandler(item);
+        item.children.forEach((item) => run.enqueued(item));
+
+        setTimeout(() => {
+          run.passed(item);
+          item.children.forEach((item) => run.passed(item));
+        }, 1000);
+      }
+      setTimeout(() => {
+        run.end();
+      }, 2500);
+    }
+  );
+
+  if (workspace.workspaceFolders?.[0].uri.path) {
+    const path = workspace.workspaceFolders[0].uri.path;
+    const dir = await readdir(path, {
+      encoding: "utf-8",
+      withFileTypes: true,
+    });
+
+    for (const f of dir) {
+      if (f.name.startsWith(".") || !f.isDirectory()) continue;
+
+      const testDir = `${path}/${f.name}/test`;
+
+      const item = testController.createTestItem(
+        f.name,
+        f.name,
+        Uri.file(testDir)
+      );
+
+      item.canResolveChildren = true;
+
+      testController.items.add(item);
+    }
+  }
+
+  const tree = window.createTreeView<string>("contest", {
+    treeDataProvider: {
+      getChildren(element) {
+        if (!rc.contest) return [];
+        return element == null
+          ? Object.keys(rc.contest).filter((key) => key !== "contest")
+          : [];
+      },
+      getTreeItem(element) {
+        return {
+          label: `${element}: ${rc.contest?.[element]}`,
+        };
+      },
+    },
+  });
+
+  await commands.executeCommand("setContext", "vs-kat.has-problems", true);
+  await commands.executeCommand("setContext", "vs-kat.contest-mode", true);
+
   // Add command to the extension context
-  context.subscriptions.push(showHelloWorldCommand);
+  context.subscriptions.push(
+    showHelloWorldCommand,
+    tree,
+    testController,
+    testProfile
+  );
 }
---
+++ src/utilities/handleRcFile.ts
@@ -0,0 +1,68 @@
+const rcKeys = [
+  "user",
+  "kattis",
+  "kat",
+  "File associations",
+  "Initialize commands",
+  "Run commands",
+  "Compile commands",
+  "Naming",
+  "Default options",
+  "contest",
+] as const;
+
+export function parseRC(
+  input: string
+): Record<(typeof rcKeys)[number], Record<string, string> | undefined> {
+  const result: Record<string, Record<string, string>> = {};
+  let intermediary: Record<string, string> = result as any;
+  for (const line of input.split(/\r?\n/)) {
+    if (!line || !line.trim() || line.startsWith("#")) continue;
+    if (line.startsWith("[")) {
+      const matches = /\[(.+)\]/.exec(line);
+      if (matches?.[1]) {
+        intermediary = {};
+        result[matches[1]] = intermediary;
+      }
+      continue;
+    }
+    const colonSplit = line.split(": ");
+    if (colonSplit.length > 1) {
+      const [key, ...rest] = colonSplit;
+      const value = rest.join(": ");
+      intermediary[key] = value;
+    } else {
+      const [rawKey, ...rest] = line.split("=");
+      const value = rest.join("=").trim();
+      intermediary[rawKey.trim()] = value;
+    }
+  }
+
+  return result;
+}
+
+export function printRC(
+  input: Record<(typeof rcKeys)[number], Record<string, string> | undefined>
+): string {
+  const warning = `# This file includes a secret token that allows you to log in.
+# DO NOT SHARE IT WITH ANYONE ELSE.
+# If someone gets access to this token, please revoke it by changing your KATTIS password.
+
+`;
+  let buffer = warning;
+
+  for (const [key, sectionOrItem] of Object.entries(input)) {
+    if (!sectionOrItem) continue;
+    if (typeof sectionOrItem === "string") {
+      buffer += `${key}: ${sectionOrItem}\n`;
+    } else {
+      buffer += `[${key}]\n`;
+      for (const [key, value] of Object.entries(sectionOrItem)) {
+        buffer += `${key}: ${value}\n`;
+      }
+      buffer += "\n";
+    }
+  }
+
+  return buffer;
+}
